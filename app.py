c7226c4eb27817c0edee9d9df8fed048795f27a4
Pnc-tpp <diamond100751@gmail.com>
2024-11-21 17:14:41 +0700

    Update app.py

--- app.py
+++ app.py
@@ -33,32 +33,27 @@ st.markdown("Please upload images to classify")
 uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"])
 
 if uploaded_file is not None:
-    # Save the uploaded file temporarily
-    temp_file_path = "temp_image.jpg"
-    with open(temp_file_path, "wb") as f:
-      f.write(uploaded_file.getbuffer())
-    from tensorflow.keras.preprocessing import image
-import numpy as np
-
-# โหลดและแปลงภาพ
-img = image.load_img(temp_file_path, target_size=(150, 150))  # กำหนดขนาดภาพให้ตรงกับโมเดล
-img_array = image.img_to_array(img)
-img_array = np.expand_dims(img_array, axis=0)  # เพิ่ม batch dimension
-img_array /= 255.0  # ปรับค่าพิกเซลเป็น 0-1
-
-# ทำนายคลาส
-predictions = model.predict(img_array)
-predicted_class = np.argmax(predictions, axis=1)[0]  # ดึงคลาสที่มีค่าความน่าจะเป็นสูงสุด
-print("Predicted class:", predicted_class)
-if predicted_class is not None:
-        st.image(uploaded_file, caption="Uploaded Image", use_column_width=True)
-        st.write(f"Predicted class: {class_labels[predicted_class]}")
-else:
-        st.error("Unable to classify the image. Please try a different image.")
-        st.write(f"Description: This images are classified as a '{class_labels[predicted_class]}' tone.")
-
-        st.success("Image classification completed successfully!")
-        os.remove(temp_file_path)
+	temp_file_path = "temp_image.jpg"
+	with open(temp_file_path, "wb") as f:
+	f.write(uploaded_file.getbuffer())
+	from tensorflow.keras.preprocessing import image
+	import numpy as np
+	img = image.load_img(temp_file_path, target_size=(150, 150))  # กำหนดขนาดภาพให้ตรงกับโมเดล
+	img_array =image.img_to_array(img)
+	img_array = np.expand_dims(img_array, axis=0)  # เพิ่ม batch dimension
+	img_array /= 255.0  # ปรับค่าพิกเซลเป็น 0-1
 	
+	predictions = model.predict(img_array)
+	predicted_class = np.argmax(predictions, axis=1)[0]  # ดึงคลาสที่มีค่าความน่าจะเป็นสูงสุด
+	print("Predicted class:", predicted_class)
+	if predicted_class is not None:
+	        st.image(uploaded_file, caption="Uploaded Image", use_column_width=True)
+	        st.write(f"Predicted class: {class_labels[predicted_class]}")
+	else:
+	        st.error("Unable to classify the image. Please try a different image.")
+	        st.write(f"Description: This images are classified as a '{class_labels[predicted_class]}' tone.")
+	
+	        st.success("Image classification completed successfully!")
+	        os.remove(temp_file_path)		
 else:
 	st.warning("Please upload an image to classify.")
